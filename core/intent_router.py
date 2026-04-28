57d52385f087f5a38c4198a2ae5a4c7dfa95e570
haricode2970 <srihariprasad7078@gmail.com>
2026-04-28 09:08:16 +0530

    fix: update LangChain imports to modular structure
    Replace langchain.schema with langchain_core.messages for
    HumanMessage and SystemMessage. Resolves ModuleNotFoundError
    on latest LangChain versions.

--- core/intent_router.py
+++ core/intent_router.py
@@ -3,7 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
